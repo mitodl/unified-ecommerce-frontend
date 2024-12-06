df169a5cf94aa3401a419de624c3eb360c597afe
James Kachel <jkachel@mit.edu>
2024-12-06 14:50:48 -0600

    Got more styling in place for the cart page and chooser stuff
    - The root page now has a chooser and a cart, and some logic to determine where you go.
    - Side-ported the Card component in and made some changes to make it work within the UE framework
    - Copied over pxToRem too
    - Got the bones set back up for the cart page
    
    Doesn't yet pull cart data, but that's arguably the easy thing.
    
    Some of the stuff in here should really go into smoot-design but my plan is to get them roughed in here first and debugged to an extent, then throw them in there (so I'm not waiting on reviews over there so much).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@
 import React from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useMetaIntegratedSystemsList } from "@/services/ecommerce/meta/hooks";
-
+import { styled } from "@mitodl/smoot-design";
+import { Typography } from "@mui/material";
 import { getCurrentSystem } from "@/utils/system";
+import { Card } from "@/page-components/Card/Card";
+
+const CartPageContainer = styled.div`
+  margin: 64px 108px;
+`
+
+const StyledCard = styled(Card)`
+  padding: 32px;
+`;
+
+const SelectSystemContainer = styled.div`
+  margin: 16px 0;
+`;
 
 const SelectSystem: React.FC = () => {
   const systems = useMetaIntegratedSystemsList();
@@ -15,7 +29,8 @@ const SelectSystem: React.FC = () => {
     router.push(`/?system=${ev.target.value}`);
   }
 
-  return systems.isFetched && systems.data ? <>
+  return <SelectSystemContainer>
+    {systems.isFetched && systems.data ? <>
     <label htmlFor="system">Select a system:</label>
     <select name="system" id="system" onChange={hndSystemChange}>
       <option value="">Select a system</option>
@@ -24,25 +39,83 @@ const SelectSystem: React.FC = () => {
           {system.name}
         </option>
       ))}
-    </select>
-  </> : <>Loading systems...</>
+    </select></> : <p>Loading systems...</p>}
+  </SelectSystemContainer>;
+}
+
+const CartContainer = styled.div`
+`;
+
+const CartBodyContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 48px;
+`;
+
+const CartHeader = styled.h1`
+  width: 100%;
+  flex-grow: 1;
+  margin-bottom: 16px;
+`;
+
+const CartItemsContainer = styled.div`
+  width: 816px;
+`;
+
+const CartSummaryContainer = styled.div`
+  width: auto;
+  flex-grow: 1;
+`;
+
+const Cart = () => {
+  return <CartContainer>
+    <CartHeader>You are about to purchase the following:</CartHeader>
+    <CartBodyContainer>
+      <CartItemsContainer>
+        <StyledCard>
+          <Card.Content>
+            <p>This is an item that's in the cart..</p>
+          </Card.Content>
+        </StyledCard>
+
+        <StyledCard>
+          <Card.Content>
+            <p>This is an item that's in the cart..</p>
+          </Card.Content>
+        </StyledCard>
+      </CartItemsContainer>
+      <CartSummaryContainer>
+        <StyledCard>
+          <Card.Content>
+            <Typography variant="h3">Order Summary</Typography>
+            
+            <p>This is an item that's in the cart..</p>
+          </Card.Content>
+          <Card.Footer>
+            By placing my order I agree to the Terms of Service and Privacy Policy.
+          </Card.Footer>
+        </StyledCard>
+
+      </CartSummaryContainer>
+    </CartBodyContainer>
+  </CartContainer>
 }
 
 const Home = () => {
   const searchParams = useSearchParams();
   const specifiedSystem = getCurrentSystem(searchParams);
 
-  return specifiedSystem === "" ? <>
-    <p>You ain't got no system.</p>
+  return <CartPageContainer>
+    {specifiedSystem === "" && <StyledCard>
+      <Card.Content>
+        We can't determine what system you're trying to access. Please choose a system to continue.
 
-    <h1>Home</h1>
-
-    <SelectSystem />
-  </> : <div>
-      <h1>Home</h1>
-
-      <p>I suppose we should show you a cart.</p>
-    </div>;
+        <SelectSystem />
+      </Card.Content>
+    </StyledCard>}
+    {specifiedSystem !== "" && <Cart />
+    }
+  </CartPageContainer>;
 };
 
 export default Home;
---
+++ src/page-components/Card/ListCard.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react"
+import { ListCard } from "./ListCard"
+import React from "react"
+
+describe("ListCard", () => {
+  test("has class MitCard-root on root element", () => {
+    const { container } = render(
+      <ListCard className="Foo">
+        <ListCard.Content>Hello world</ListCard.Content>
+      </ListCard>,
+    )
+    const card = container.firstChild
+
+    expect(card).toHaveClass("MitListCard-root")
+    expect(card).toHaveClass("Foo")
+  })
+})
